b14991848e702290d334e8cb9101e3182d0e2ac3
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-05-12 21:12:41 -0300

    completed github chalenge with event listener, colors and conditions

--- Spread Fullstack Developer/5-Introdução ao js/2-criando contador/assets/js/script.js
+++ Spread Fullstack Developer/5-Introdução ao js/2-criando contador/assets/js/script.js
@@ -2,11 +2,39 @@ let currentNumberWrapper = document.getElementById('currentNumber');
 let currentNumber = 0;
 
 function increment() {
-	currentNumber += 1;
+	if (currentNumber < 10) {
+		currentNumber += 1;
+	} else {
+		alert('número máximo 10');
+	}
+
 	currentNumberWrapper.innerHTML = currentNumber;
+	setColor(currentNumber);
 }
 
 function decrement() {
-	currentNumber -= 1;
+	if (currentNumber > -10) {
+		currentNumber -= 1;
+	} else {
+		alert('número mínimo -10');
+	}
 	currentNumberWrapper.innerHTML = currentNumber;
+	setColor(currentNumber);
 }
+
+function setColor(number) {
+	if (number > 0) {
+		currentNumberWrapper.style.color = 'green';
+		console.log('green');
+	}
+	else if (number < 0) {
+		currentNumberWrapper.style.color  = 'red';
+		console.log('red');
+	} else {
+		currentNumberWrapper.style.color  = 'gray';
+		console.log('black');
+	}
+}
+
+document.getElementsByName('adicionar')[0].addEventListener('click', increment)
+document.getElementsByName('subtrair')[0].addEventListener('click', decrement)
