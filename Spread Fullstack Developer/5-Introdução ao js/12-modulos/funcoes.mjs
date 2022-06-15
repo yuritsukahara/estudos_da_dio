33a89b4c86b489f3e6358155c5449f1b3f3d1619
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-06-15 18:54:05 -0300

    desafio de modulos

---
+++ Spread Fullstack Developer/5-Introdução ao js/12-modulos/funcoes.mjs
@@ -0,0 +1,19 @@
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`;
+}
+
+
+function mostraCidade(nome, cidade){
+    return `A cidade de ${nome} é ${cidade}`;
+}
+
+
+function mostraHobby(nome, hobby){
+    return `A hobby de ${nome} é ${hobby}`;
+}
+
+export{
+    mostraCidade,
+    mostraHobby,
+    mostraIdade,
+}
---
+++ Spread Fullstack Developer/5-Introdução ao js/12-modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Yuri', 29));
+console.log(mostraHobby('Yuri', 'Desenhar'));
+console.log(mostraCidade('Yuri', 'Apucarana'));
