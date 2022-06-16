53ccf606f7489aaf8d4c421813785bbed6e07ca3
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-06-16 18:10:27 -0300

    estudo de Interfaces

--- Spread Fullstack Developer/5-Introdução ao js/14-introdução ao TypeScript/1-typescript/index.ts
+++ Spread Fullstack Developer/5-Introdução ao js/14-introdução ao TypeScript/1-typescript/index.ts
@@ -1,1 +1,28 @@
-console.log('TypeScript')
+ // types
+ // interfaces
+
+ interface IAnimal{
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRugido(alturaEmDecibeis: number): void;
+ }
+
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean;
+}
+
+ const ANIMAL: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'aquático',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+ }
+
+ANIMAL.executarRugido(10)
+
+ const FELINO: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+
+ }
