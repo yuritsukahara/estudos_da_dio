542d893aa75c6829d34339c023253dae04375ca5
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-06-30 20:28:27 -0300

    Revert "deleted folder, will start from 0"
    This reverts commit 9a030430adf4aca46d97613a47cb1e23fa1f7077.

---
+++ Spread Fullstack Developer/6-reactJs/2-introdução aos react hooks/quotes/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const buttonA = <button>Histórico dos clientes</button>
+const buttonB = <button>Cadastrar Cliente</button>
+
+const hasCustomer = false;
+
+const App = () => {
+
+	const renderShowHistory = (
+		<div>
+		Clique no botão abaixo para visualizar o histórico dos clientes.
+		<br />
+		{buttonA}
+		</div>
+	)
+
+	const renderAddCustomer = (
+		<div>
+					Clique abaixo para cadastar o clientes
+					<br />
+					{buttonB}
+				</div>
+	)
+
+	const showCustomer = () => {
+		if (!hasCustomer) return null
+		return (
+			<div>
+				<h1>Nome do Cliente: Aristeu Ninguém</h1>
+			</div>
+		)
+	}
+
+	return (
+		<div>
+			<p>Digital Innovation One</p>
+			<p>Bem vindo a nossa aula =D</p>
+			Valor da variável hasCustomer: {hasCustomer.toString()}
+			{hasCustomer ? renderShowHistory : renderAddCustomer}
+			<div>
+				{showCustomer()}
+			</div>
+		</div>
+	);
+};
+export default App;
