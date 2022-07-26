8ac853cd0ec741598f0276a8af080461a2c7a90e
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-07-25 23:45:08 -0300

    added user interface

--- api-dio-node/src/controllers/CreateUserController.ts
+++ api-dio-node/src/controllers/CreateUserController.ts
@@ -1,14 +1,18 @@
 import { Request, response, Response } from 'express';
-
+import { CreateUserService } from '../services/CreateUserServices';
 class CreateUserController {
 	handle(req: Request, res: Response) {
+		const createUserService = new CreateUserService();
 		const nome = req.body.nome;
+		const email = req.body.email;
+
 		if (nome.length === 0) {
 			return res
 				.status(404)
 				.json({ mensagem: 'Informe um nome de usuário' });
 		}
-		return res.status(201).json({ mensagem: `Usuário ${nome} criado` });
+		const user = createUserService.execute({ nome, email });
+		return res.status(201).json({ user });
 	}
 }
 
