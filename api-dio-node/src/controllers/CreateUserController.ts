bc1ef051a7fdf22a467be21dfa93007cfaaaa0d8
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-07-25 23:32:54 -0300

    created 400 status

--- api-dio-node/src/controllers/CreateUserController.ts
+++ api-dio-node/src/controllers/CreateUserController.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 
 class CreateUserController {
 	handle(req: Request, res: Response) {
 		const nome = req.body.nome;
-
+		if (nome.length === 0) {
+			return res
+				.status(404)
+				.json({ mensagem: 'Informe um nome de usuário' });
+		}
 		return res.status(201).json({ mensagem: `Usuário ${nome} criado` });
 	}
 }
